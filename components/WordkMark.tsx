d06e9135363d7f3b1c10648bba2cb6c51602fdf0
Jeroen Gerits <jeroen@gerits.email>
2022-10-05 20:44:10 +0200

    nav issue

--- components/WordkMark.tsx
+++ components/WordkMark.tsx
@@ -4,8 +4,8 @@ type Props = {
 const WordkMark = () => {
     return (
         <>
-            <h1 className="text-6xl select-none font-serif text-green-50/80 mb-2 font-black">The Happy Stoner</h1>
-            <h2 className="select-none text-lg uppercase text-green-50/50 tracking-widest font-light">Amsterdam Finest
+            <h1 className="text-5xl lg:text-6xl select-none font-serif text-green-50/80 mb-2 font-black">The Happy Stoner</h1>
+            <h2 className="select-none text-base lg:text-lg uppercase text-green-50/50 tracking-widest font-light">Amsterdam Finest
                 Stash
                 Movement</h2>
         </>
