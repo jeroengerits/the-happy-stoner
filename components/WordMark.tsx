9ea75d865cd3d2fab82838ca0f3cfb5807534bb2
Jeroen Gerits <jeroen@gerits.email>
2022-10-06 16:54:43 +0200

    refactor

--- components/WordMark.tsx
+++ components/WordMark.tsx
@@ -6,7 +6,7 @@ type Props = {} & React.HTMLAttributes<HTMLDivElement>;
 const WordMark = ({ className, ...rest }: Props) => (
   <div className={classNames("block text-center", className)} {...rest}>
     <h1 className="heading-lg select-none text-green-50">The Happy Stoner</h1>
-    <h2 className="select-none text-xs font-light uppercase tracking-wide  lg:text-base lg:tracking-widest xl:text-lg">
+    <h2 className="select-none text-xs font-light uppercase tracking-wide  lg:text-sm lg:tracking-widest">
       Amsterdam Finest Stash Movement
     </h2>
   </div>
