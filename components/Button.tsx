f3a66b4da8743446dd510626c6e306f0134a9d46
Jeroen Gerits <jeroen@gerits.email>
2022-10-06 12:05:43 +0200

    refactor

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Button = ({ children, ...rest }: Props) => {
   const className = classNames(
-    "focus:outline-none focus:ring-2 ring-white/50 whitespace-nowrap select-none bg-white/10 hover:bg-white/20 text-white/80 tracking-wide font-bold transition-all rounded-md py-3 px-8"
+    "focus:outline-none focus:ring-2 ring-white/50 whitespace-nowrap select-none bg-white/10 hover:bg-white/20 text-white/60 hover:text-white-80 tracking-wide font-bold transition-all rounded-md py-3 px-8"
   );
   return (
     <button className={className} {...rest}>
