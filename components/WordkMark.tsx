debbed16b9866cf17727e4a9602686211e866cf0
Jeroen Gerits <jeroen@gerits.email>
2022-10-06 09:26:30 +0200

    Some

--- components/WordkMark.tsx
+++ components/WordkMark.tsx
@@ -1,23 +1,22 @@
 import classNames from "classnames";
-import classnames from "classnames";
 
 type Props = {
-    size: "small" | "normal" | "large";
+  size: "small" | "normal" | "large";
 };
 
 const WordkMark = () => {
-    const titleClassName = classNames("text-3xl md:text-4xl lg:text-5xl select-none font-serif text-green-50/80 mb-1 lg:mb-2 font-black")
-    const subtitleClassName = classNames("select-none text-xs md:text-sm uppercase text-green-50/80 tracking-widest font-light")
-    return (
-        <>
-            <h1 className={titleClassName}>
-                The Happy Stoner
-            </h1>
-            <h2 className={subtitleClassName}>
-                Amsterdam Finest Stash Movement
-            </h2>
-        </>
-    );
+  const titleClassName = classNames(
+    "text-3xl md:text-4xl lg:text-5xl select-none font-serif text-green-50/80 mb-1 lg:mb-2 font-black"
+  );
+  const subtitleClassName = classNames(
+    "select-none text-xs md:text-sm uppercase text-green-50/80 tracking-widest font-light"
+  );
+  return (
+    <>
+      <h1 className={titleClassName}>The Happy Stoner</h1>
+      <h2 className={subtitleClassName}>Amsterdam Finest Stash Movement</h2>
+    </>
+  );
 };
 
 export default WordkMark;
