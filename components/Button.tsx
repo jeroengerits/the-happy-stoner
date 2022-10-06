fa2e6c04288e1fbc4c0b0827438260d4885aa7fc
Jeroen Gerits <jeroen@gerits.email>
2022-10-06 16:59:03 +0200

    some refactor

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,18 +5,16 @@ type Props = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className, ...rest }: Props) => {
-  return (
-    <button
-      className={classNames(
-        "select-none  whitespace-nowrap rounded-md bg-black/10 py-4 px-8 font-bold tracking-wide text-green-50 ring-green-200 transition-all hover:bg-green-900 hover:font-black hover:text-white focus:outline-none focus:ring-2 active:bg-green-700/50",
-        className
-      )}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = ({ children, className, ...rest }: Props) => (
+  <button
+    className={classNames(
+      "select-none  whitespace-nowrap rounded-md bg-black/10 py-4 px-8 font-bold tracking-wide text-green-50 ring-green-200 transition-all hover:bg-green-900 hover:font-black hover:text-white focus:outline-none focus:ring-2 active:bg-green-700/50",
+      className
+    )}
+    {...rest}
+  >
+    {children}
+  </button>
+);
 
 export default Button;
