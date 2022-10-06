eab84fb24cf1a0ece7a773ae087d043f72cdcbde
Jeroen Gerits <jeroen@gerits.email>
2022-10-06 13:10:15 +0200

    refactor

--- components/WordMark.tsx
+++ components/WordMark.tsx
@@ -11,7 +11,7 @@ const WordMark = ({ className, ...rest }: Props) => (
     <h1 className="select-none font-serif text-3xl font-black text-white/80 md:mb-1 md:text-4xl lg:mb-2 lg:text-5xl">
       The Happy Stoner
     </h1>
-    <h2 className="select-none text-xs font-light uppercase tracking-widest text-white/80 md:text-sm lg:text-base">
+    <h2 className="select-none text-xs font-light uppercase tracking-widest text-white/80 lg:text-sm xl:text-base">
       Amsterdam Finest Stash Movement
     </h2>
   </div>
